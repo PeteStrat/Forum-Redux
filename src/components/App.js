c111cfb338ce0080c4427f40976b3e14c3d87738
PeteStrat <Stradakis@gmail.com>
2018-03-24 17:57:08 -0400

    Adding propType validation to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from '../logo.svg';
 import '../App.css';
 import { withRouter, Route, Switch } from 'react-router-dom';
+import propTypes from 'prop-types';
 import NavigationBar from './NavigationBar';
 import Category from './Category';
 import ErrorPage from './ErrorPage';
@@ -12,23 +13,10 @@ import { getCategories } from '../actions';
 class App extends Component {
   componentDidMount () {
     this.props.getCategories();
-    // this.setState((prevState) => {
-    //   // this.props.getCategories
-    //   return this.props.getCategories;
-    // });
-    // const url = 'http://localhost:3001/categories';
-    // axios.get(url, {
-    //   headers: { 'Authorization': '123' }
-    // })
-    //   .then((response) => {
-    //     let categoryNames = response.data.categories.map(category => category.name);
-    //     this.setState({categories: categoryNames});
-    //   });
   }
 
   render () {
     let categories = [];
-    // console.log(this.state);
     console.log(this.props);
     if (this.props.categories) {
       categories = this.props.categories;
@@ -67,6 +55,11 @@ let mapStateToProps = (state) => {
   return {
     categories: state.categories.categoriesArray
   };
+};
+
+App.propTypes = {
+  categories: propTypes.array,
+  getCategories: propTypes.func.isRequired
 };
 
 export default withRouter(connect(mapStateToProps, {getCategories})(App));
